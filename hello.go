94c93df8c4b2d243acfda1323bdd10b6a34aac3c
Vu Van Huy STC <huyvv@vnpt-technology.vn>
2022-09-11 22:17:50 +0700

    init project

---
+++ hello.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+const spanish = "Spanish"
+const french = "French"
+
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) (result string) {
+	if name == "" {
+		name = "World"
+	}
+	result = greetingPrefix(language) + name
+	return
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
